e8ec30262d46195112ba5bf40cbdbe11d2174410
Neil Elder <neil@saminfo.com>
2015-04-10 10:12:48 -0400

    Added extensions class

---
+++ LinqSnippets/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSnippets
+{
+    static class Extensions
+    {
+        public static string UpperCaseFirstCharacter(this String value)
+        {
+            char[] charArray = value.ToCharArray();
+            var firstChar = charArray[0].ToString().ToUpper();
+            charArray[0] = Convert.ToChar(firstChar);
+            return new string(charArray);
+        }
+    }
+}
